a1a082bf507a20aee55e98549259664c19365665
Klivinsthon Kendert <contato_klivinsthon@outlook.com>
2022-07-11 15:02:30 -0300

    Lifecycle e states

--- src/components/planets.js
+++ src/components/planets.js
@@ -1,23 +1,28 @@
 import React, { Fragment } from "react";
 import PlanetBase from "./planetBase";
 
+async function getPlanets() {
+    let response = await fetch("http://localhost:3000/planets-api/planets.json")
+    let planetData = await response.json()
+    return planetData
+}
+
 class Planets extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            planets: [
-                {
-                    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg",
-                    name: "Mercúrio",
-                    description: "Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres.",
-                },
-                {
-                    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg/300px-Jupiter_and_its_shrunken_Great_Red_Spot.jpg",
-                    name: "Júpiter",
-                    description: "Júpiter é o maior planeta do Sistema Solar, tanto em diâmetro quanto em massa, e é o quinto mais próximo do Sol.",
-                }
-            ]
+            planets: []
         }
+    }
+
+    componentDidMount() {
+        getPlanets().then(
+            planetData => {
+                this.setState({
+                    planets: this.state.planets = planetData["planets"]
+                })
+            }
+        )
     }
 
     render() {
@@ -28,7 +33,7 @@ class Planets extends React.Component {
                         (s, i) => {
                             return (
                                     <PlanetBase
-                                        img={s.img}
+                                        img={s.img_url}
                                         name={s.name}
                                         description={s.description}
                                         key={i}
